1ac9a7aa0f69affef782a8d7acd3c888cbaf5b3a
Evan <evan.wang@tkspring.com>
2026-01-19 12:39:25 +0800

    fix script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,8 +12,9 @@ import {DepositVault} from "contracts/DepositVault.sol";
 import {MTokenInitParams, ReceiversInitParams, InstantInitParams} from "contracts/interfaces/IManageableVault.sol";
 import {AggregatorV3Mock} from "contracts/mocks/AggregatorV3Mock.sol";
 import {SanctionsListMock} from "contracts/mocks/SanctionsListTest.sol";
+import {DataFeed} from "contracts/feeds/DataFeed.sol";
 
-contract DeployMarketsScript is Script {
+contract DeployScript is Script {
     using StringHelper for string;
 
     string public network;
@@ -21,6 +22,19 @@ contract DeployMarketsScript is Script {
     address public adminAddr;
     address public deployerAddr;
     bool isMainnet;
+
+    MidasAccessControl public midasAccessControl;
+    SanctionsListMock public sanctionsList;
+
+    ERC20Mock paymentToken;
+    ERC20Mock mToken;
+    AggregatorV3Mock paymentTokenPricefeed;
+    AggregatorV3Mock mTokenPricefeed;
+    DataFeed paymentTokenDataFeed;
+    DataFeed mTokenDataFeed;
+    DepositVault depositVault;
+
+    address tokenReceiver = 0x4479B26363c0465EE05A45ED13B4fAeA3E8b009A;
 
     function setUp() public {
         // Load network from environment variable
@@ -52,58 +66,138 @@ contract DeployMarketsScript is Script {
 
         bytes memory data = abi.encodeWithSignature("initialize()");
 
-        ERC1967Proxy ac_proxy = new ERC1967Proxy(
+        midasAccessControl = MidasAccessControl(address(new ERC1967Proxy(
             address(impl), data
-        );
-        console.log("MidasAccessControl deployed at:", address(ac_proxy));
+        )));
+        console.log("MidasAccessControl deployed at:", address(midasAccessControl));
 
         if (!isMainnet) {
-            ERC20Mock mockToken = new ERC20Mock(6);
-            console.log("MockToken deployed at:", address(mockToken));
+            mToken = new ERC20Mock(18);
+            console.log("mToken deployed at:", address(mToken));
 
-            mockToken.mint(deployerAddr, 1_000_000 * 10 ** mockToken.decimals());
-            console.log("Minted 1,000,000 MTK to deployer");
+            // add payment token to deposit vault
+            paymentToken = new ERC20Mock(6);
+            console.log("Deployed mock payment token at:", address(paymentToken));
+            paymentToken.mint(deployerAddr, 1_000_000e6); // mint 1,000,000 USDC to deployer
+            console.log("Minted 1,000,000 paymentToken to deployer");
 
-            AggregatorV3Mock dataFeed = new AggregatorV3Mock();
-            console.log("AggregatorV3Mock deployed at:", address(dataFeed));
-            dataFeed.setRoundData(1 * 10 ** 8); // Set price to 1 MTK = $1
+            mTokenPricefeed = new AggregatorV3Mock();
+            console.log("mToken pricefeed deployed at:", address(mTokenPricefeed));
+            mTokenPricefeed.setRoundData(1 * 10 ** 8); // Set price to 1 MTK = $1
+
+            paymentTokenPricefeed = new AggregatorV3Mock();
+            console.log("paymentToken pricefeed deployed at:", address(paymentTokenPricefeed));
+            paymentTokenPricefeed.setRoundData(1 * 10 ** 8); // Set price to 1 MTK = $1
+
+            {
+                DataFeed feedsImpl = new DataFeed();
+                console.log("Deployed DataFeed impl at:", address(feedsImpl));
+            
+                uint256 _healthyDiff = type(uint256).max;
+                int256 _minExpectedAnswer = 1;
+                int256 _maxExpectedAnswer = type(int256).max;
+                data = abi.encodeWithSignature(
+                    "initialize(address,address,uint256,int256,int256)",
+                    address(midasAccessControl),
+                    address(mTokenPricefeed),
+                    _healthyDiff,
+                    _minExpectedAnswer,
+                    _maxExpectedAnswer
+                );
+                mTokenDataFeed = DataFeed(address(new ERC1967Proxy(
+                    address(feedsImpl), data
+                )));
+                console.log("mToken data feed deployed at:", address(mTokenDataFeed));
+
+                data = abi.encodeWithSignature(
+                    "initialize(address,address,uint256,int256,int256)",
+                    address(midasAccessControl),
+                    address(paymentTokenPricefeed),
+                    _healthyDiff,
+                    _minExpectedAnswer,
+                    _maxExpectedAnswer
+                );
+                paymentTokenDataFeed = DataFeed(address(new ERC1967Proxy(
+                    address(feedsImpl), data
+                )));
+                console.log("paymentToken data feed deployed at:", address(paymentTokenDataFeed));
+            }
 
             DepositVault vaultImpl = new DepositVault();
             console.log("DepositVault impl deployed at:", address(vaultImpl));
 
-            uint256 _variationTolerance = 50; // 0.5%
-            uint256 _minAmount = 10 * 10 ** mockToken.decimals();
-            uint256 _minMTokenAmountForFirstDeposit = 100 * 10 ** mockToken.decimals();
-            uint256 _maxSupplyCap = 1_000_000 * 10 ** mockToken.decimals();
-            SanctionsListMock _sanctionsList = new SanctionsListMock();
-            console.log("SanctionsListMock deployed at:", address(_sanctionsList));
+            {
+                uint256 _variationTolerance = 50; // 0.5%
+                uint256 _minAmount = 10 ether;
+                uint256 _minMTokenAmountForFirstDeposit = 10 ether;
+                uint256 _maxSupplyCap = 1_000_000 ether;
+                SanctionsListMock _sanctionsList = new SanctionsListMock();
+                console.log("SanctionsListMock deployed at:", address(_sanctionsList));
 
-            bytes memory initData = abi.encodeWithSelector(
-                DepositVault.initialize.selector,
-                address(ac_proxy),
-                MTokenInitParams({
-                    mToken: address(mockToken),
-                    mTokenDataFeed: address(dataFeed)
-                }),
-                ReceiversInitParams({
-                    tokensReceiver: deployerAddr,
-                    feeReceiver: deployerAddr
-                }),
-                InstantInitParams({
-                    instantFee: 0,
-                    instantDailyLimit: 10_000 * 10 ** mockToken.decimals()
-                }),
-                address(_sanctionsList),
-                _variationTolerance,
-                _minAmount,
-                _minMTokenAmountForFirstDeposit,
-                _maxSupplyCap
-            );
+                bytes memory initData = abi.encodeWithSelector(
+                    DepositVault.initialize.selector,
+                    address(midasAccessControl),
+                    MTokenInitParams({
+                        mToken: address(mToken),
+                        mTokenDataFeed: address(mTokenDataFeed)
+                    }),
+                    ReceiversInitParams({
+                        tokensReceiver: tokenReceiver,
+                        feeReceiver: tokenReceiver
+                    }),
+                    InstantInitParams({
+                        instantFee: 0,
+                        // use 18 decimals for instant limits
+                        instantDailyLimit: 1000_000 ether
+                    }),
+                    address(_sanctionsList),
+                    _variationTolerance,
+                    _minAmount,
+                    _minMTokenAmountForFirstDeposit,
+                    _maxSupplyCap
+                );
+                depositVault = DepositVault(address(new ERC1967Proxy(
+                    address(vaultImpl), initData
+                )));
+                console.log("DepositVault deployed at:", address(depositVault));
+            }
 
-            ERC1967Proxy vaultProxy = new ERC1967Proxy(
-                address(vaultImpl), initData
+            midasAccessControl.grantRole(
+                depositVault.vaultRole(),
+                deployerAddr
             );
-            console.log("DepositVault deployed at:", address(vaultProxy));
+            console.log("Granted vault role to deployer");
+
+            uint256 tokenFee = 0;
+            uint256 allowance = type(uint256).max;
+            bool stable = true;
+            depositVault.addPaymentToken(address(paymentToken), address(paymentTokenDataFeed),
+                tokenFee, allowance, stable
+            );
+            console.log("Added payment token to DepositVault");
+
+            // Approve depositVault to spend paymentToken
+            uint256 depositAmount = 100e6; // 100 USDC
+            paymentToken.approve(address(depositVault), depositAmount);
+            // Make a deposit
+            uint256 minReceiveAmount = 100e6; // 100 * 1e6 (assuming mToken has 6 decimals)
+            bytes32 referrerId;
+            // deposit amount must convert to 18 decimals inside depositInstant
+            depositVault.depositInstant(address(paymentToken), depositAmount * 1e12, minReceiveAmount * 1e12, referrerId
+            );
+            console.log("Deposited", depositAmount, "payment tokens to DepositVault");
+
+            uint256 mTokenBalance = mToken.balanceOf(deployerAddr);
+            console.log("Deployer mToken balance after deposit:", mTokenBalance);
+
+            uint256 paymentTokenBalance = paymentToken.balanceOf(deployerAddr);
+            console.log("Deployer paymentToken balance after deposit:", paymentTokenBalance);
+
+            uint256 paymentTokenInVault = paymentToken.balanceOf(address(depositVault));
+            console.log("DepositVault paymentToken balance after deposit:", paymentTokenInVault);
+
+            uint256 paymentTokenInReceiver = paymentToken.balanceOf(tokenReceiver);
+            console.log("Tokens receiver paymentToken balance after deposit:", paymentTokenInReceiver);
         }
 
         vm.stopBroadcast();
